04a4365a4a904f1e1efa04ee036067d181d0cb14
Chase Seibert <cseibert@dropbox.com>
2019-08-22 10:56:23 -0700

    Basic histogram, no bucketing

--- git-stats.py
+++ git-stats.py
@@ -1,7 +1,10 @@
 import argparse
+import datetime
 from collections import Counter, defaultdict
 import subprocess
 import sys
+
+import colorama
 
 import settings
 
@@ -20,6 +23,7 @@ def _extract_num(str, word):
 
 def get_stats(args):
     authors = defaultdict(Counter)
+    author_added_per_day = defaultdict(lambda: defaultdict(int))
     emails = settings.TEAMS[args.team]
     stdout = get_git_stdout(args.path, emails, args.days)
     lines = stdout.split('\n')[:-1]  # empty line at end
@@ -38,31 +42,53 @@ def get_stats(args):
             files=files,
             added=added,
             deleted=deleted,
-
         ))
+        day_str = date[0:10]
+        day = datetime.datetime.strptime(day_str, '%Y-%m-%d').date()
+        author_added_per_day[author][day] = added
         if not args.just_totals:
-            print '%s %s %s' % (date[0:10], author, subject)
+            print '%s %s %s' % (day, author, subject)
 
     if not authors:
         return
 
     authors_sorted = sorted(authors.items(), key=lambda x: x[1]['added'], reverse=True)
-    print '{:}{: <30} {: >12} {: >12} {: >12}'.format(
+    print '{:}{: <30} {: >12} {: >12} {: >12}  {: <30}'.format(
         '' if args.just_totals else '\n',
         '=== Totals ===',
         'Added',
         'Deleted',
         'Files',
+        'Histogram',
     )
     for email, stats in authors_sorted:
-        print '{: <30} {: >12} {: >12} {: >12}'.format(
+        print '{: <30} {: >12} {: >12} {: >12}  {: <30}'.format(
         #print '%s: %s files, %s lines added, %s lines deleted' % (
             email,
             stats['added'],
             stats['deleted'],
             stats['files'],
+            get_histogram(author_added_per_day[email], args.days),
         )
 
+
+def get_histogram(author_added_per_day, num_days, buckets=30):
+    start_date = (datetime.datetime.now() - datetime.timedelta(days=num_days)).date()
+    histogram = ''
+    # need to account for empty days
+    for day_num in range(num_days):  # TODO buckets
+        current_date = start_date + datetime.timedelta(days=day_num)
+        added = author_added_per_day.get(current_date, 0)
+        char = '.' if added == 0 else '#'
+        color = colorama.Style.DIM + colorama.Fore.WHITE
+        if added > 10:
+            color = colorama.Style.NORMAL
+        if added > 100:
+            color = colorama.Style.BRIGHT
+        if added > 1000:
+            color = colorama.Style.BRIGHT + colorama.Fore.RED
+        histogram += color + char
+    return histogram
 
 
 def get_git_stdout(path, emails, days):
@@ -94,13 +120,13 @@ def get_git_stdout(path, emails, days):
 
 
 if __name__ == '__main__':
-    #colorama.init(autoreset=True)
+    colorama.init(autoreset=True)
     parser = argparse.ArgumentParser(prog='git-stats')
     parser.add_argument('--team', help='Which team from settings to use',
         **kwargs_or_default(settings.DEFAULT_TEAM))
     parser.add_argument('--path', help='Path to local git repo',
         **kwargs_or_default(settings.DEFAULT_GIT_PATH))
-    parser.add_argument('--days', help='How many days back to go', default=30)
+    parser.add_argument('--days', help='How many days back to go', default=30, type=int)
     parser.add_argument('--just-totals', help='Just print the totals', action='store_true')
     args = parser.parse_args()
     get_stats(args)
